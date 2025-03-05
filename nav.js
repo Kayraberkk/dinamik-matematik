8fde44b1a53ead15405299694bfb8dbfadd95af1
Kayraberkk <164743728+Kayraberkk@users.noreply.github.com>
2025-03-05 22:31:03 +0300

    Update nav.js

--- nav.js
+++ nav.js
@@ -8,7 +8,7 @@ const base = "https://kayraberkk.github.io/dinamik-matematik/";
 const baseTag = document.getElementById("baseTag");
 
 function changeBaseTag() {
-  if (location.hostname != "localhost" && location.hostname != "127.0.0.1") {
+  if (location.hostname != "localhost" && location.hostname != "127.0.0.1" && baseTag != null) {
     baseTag.href = base;
   }
 }
